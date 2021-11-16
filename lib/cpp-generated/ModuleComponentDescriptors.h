9a0e5b57f30f921e233517e1973c4fb7eacafed2
Szymon <szymon20000@gmail.com>
2021-11-16 16:25:54 +0100

    first measure

--- lib/cpp-generated/ModuleComponentDescriptors.h
+++ lib/cpp-generated/ModuleComponentDescriptors.h
@@ -16,7 +16,51 @@
 namespace facebook {
 namespace react {
 
-using ModuleComponentDescriptor = ConcreteComponentDescriptor<ModuleShadowNode>;
+class ModuleComponentDescriptor : public ConcreteComponentDescriptor<ModuleShadowNode> {
+public:
+    ModuleComponentDescriptor(ComponentDescriptorParameters const &parameters): ConcreteComponentDescriptor(parameters) {
+        
+    }
+    
+    // TODO think about it 
+    virtual ShadowNode::Shared createShadowNode(
+        const ShadowNodeFragment &fragment,
+        ShadowNodeFamily::Shared const &family) const override {
+      auto shadowNode =
+            std::make_shared<ModuleShadowNode>(ShadowNodeFragment{fragment.props, nullptr}, family, getTraits());
+      ShadowNode::SharedListOfShared registeredComponents = fragment.children;
+            //
+          /*  for (std::shared_ptr<const ShadowNode> sn : (*(fragment.children))) {
+                int x = 5;
+                int y = x;
+            }*/
+            //const std::shared_ptr<const ShadowNode> sn = fragment.children[0];
+            
+            //
+      adopt(shadowNode);
+
+      return shadowNode;
+    }
+    
+    // TODO fix this
+    virtual ShadowNode::Unshared cloneShadowNode(
+        const ShadowNode &sourceShadowNode,
+        const ShadowNodeFragment &fragment) const override {
+            auto shadowNode = std::make_shared<ModuleShadowNode>(sourceShadowNode, ShadowNodeFragment{});
+        ShadowNode::SharedListOfShared registeredComponents = fragment.children;
+        //
+        const std::shared_ptr<const ShadowNode> sn = (*registeredComponents)[0];
+        int x = 5;
+        int y = x;
+        //
+
+        adopt(shadowNode);
+        return shadowNode;
+    }
+    
+    virtual ~ModuleComponentDescriptor(){}
+};
+
 
 } // namespace react
 } // namespace facebook
