0bc5602dd505ddf66813b49c3a940a444e8a30b1
Szymon <szymon20000@gmail.com>
2021-11-18 19:15:59 +0100

    replace children

--- lib/cpp-generated/ShadowNodeCopyMachine.hpp
+++ lib/cpp-generated/ShadowNodeCopyMachine.hpp
@@ -8,7 +8,6 @@
 #ifndef ShadowNodeCopyMachine_hpp
 #define ShadowNodeCopyMachine_hpp
 
-#include "ModuleEventEmitters.h"
 #include "ModuleProps.h"
 #include <react/renderer/components/view/ConcreteViewShadowNode.h>
 #include "LayoutContext.h"
