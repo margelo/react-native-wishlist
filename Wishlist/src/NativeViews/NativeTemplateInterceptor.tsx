9f9112565ffc5d3655f837dbab512b137d47286f
Szymon <szymon20000@gmail.com>
2022-09-10 13:05:58 +0200

    works

--- Wishlist/src/NativeViews/NativeTemplateInterceptor.tsx
+++ Wishlist/src/NativeViews/NativeTemplateInterceptor.tsx
@@ -3,9 +3,8 @@
 // Automatically generated from components_success/COMMANDS_AND_EVENTS_TYPES_EXPORTED.flow.js
 // (/react-native/packages/react-native-codegen/src/parsers/flow/components/__test_fixtures__/fixtures.js)
 
-import {ViewProps} from 'react-native-tscodegen-types';
-import {HostComponent} from 'react-native-tscodegen-types';
-import {codegenNativeComponent} from 'react-native-tscodegen-types';
+import {ViewProps} from 'react-native';
+import codegenNativeComponent from 'react-native/Libraries/Utilities/codegenNativeComponent';
 'use strict';
 
 import {NativeModules} from 'react-native';
@@ -15,6 +14,5 @@ export type NativeTemplateInterceptorProps = Readonly<ViewProps & {
   inflatorId: string;
 }>;
 
-type NativeType = HostComponent<NativeTemplateInterceptorProps>;
 
-export default (codegenNativeComponent<NativeTemplateInterceptorProps>('MGTemplateInterceptorComponent') as NativeType);
+export default (codegenNativeComponent<NativeTemplateInterceptorProps>('MGTemplateInterceptorComponent'));
