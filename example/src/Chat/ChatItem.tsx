6d522642358aad0f168ebc4d1843bbd19d2a06ff
Szymon <szymon20000@gmail.com>
2022-11-16 12:15:17 +0100

    stash work

--- example/src/Chat/ChatItem.tsx
+++ example/src/Chat/ChatItem.tsx
@@ -94,8 +94,10 @@ export const ChatItemView: React.FC<Props> = ({ type, onAddReaction }) => {
   const data = useData<ChatItem>();
 
   const likeItemListener = useWorkletCallback((value) => {
-    value.update();
+    _log('ooo clicked!!!');
+
     data().update((dataCopy) => {
+      _log('ooo update update!!');
       const oldValue = dataCopy.get(value.key);
       oldValue.liked = !oldValue.liked;
       dataCopy.set(value.key, oldValue);
