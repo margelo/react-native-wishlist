fdeedb5fe72d76682561f1eb0680545fc320cafa
Janic Duplessis <janicduplessis@gmail.com>
2022-12-15 12:13:12 -0500

    fix: update rn worklets

--- example/babel.config.js
+++ example/babel.config.js
@@ -5,7 +5,13 @@ module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
   plugins: [
     ['@react-native/babel-plugin-codegen'],
-    ['react-native-worklets/plugin'],
+    [
+      'react-native-worklets/plugin',
+      {
+        globals: ['_log', '_chronoNow'],
+        functionsToWorkletize: [{ name: 'useTemplateValue', args: [0] }],
+      },
+    ],
     [
       'module-resolver',
       {
