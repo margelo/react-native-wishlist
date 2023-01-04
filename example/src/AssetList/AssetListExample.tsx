6b61e3889306f83d61a3beb2b0cac421e230de2f
Terry Sahaidak <tasssik@gmail.com>
2023-01-04 12:45:06 -0800

    feat: add proper list divider

--- example/src/AssetList/AssetListExample.tsx
+++ example/src/AssetList/AssetListExample.tsx
@@ -1,41 +1,75 @@
-import React, { useCallback, useState } from 'react';
+import React, { useCallback, useMemo, useState } from 'react';
 import { StyleSheet, View } from 'react-native';
+import { useWorkletCallback } from 'react-native-reanimated';
 import { WishList } from 'wishlist';
 import { AssetItem } from './AssetItem';
 import { AssetListHeader } from './AssetListHeader';
+import { AssetListSeparator } from './AssetListSeparator';
 
 import data, { AssetItemType } from './assets';
 import { Header } from './Header';
+type WithType<TType extends String, T> = { type: TType } & T;
 
-const tokens: Array<AssetItemType | { type: 'list-header' }> = data.map(
-  (item) => ({
-    ...item,
-    type: 'asset',
-  }),
-);
-tokens.unshift({ type: 'list-header' });
+type AssetListItemType = WithType<'asset', AssetItemType>;
+
+const tokens: AssetListItemType[] = data.map((item) => ({
+  ...item,
+  type: 'asset',
+}));
+
+type ListItemsType =
+  | AssetListItemType
+  | { type: 'asset-list-separator' }
+  | { type: 'asset-list-header' };
 
 export const AssetListExample: React.FC<{}> = () => {
   const [isEditing, setIsEditing] = useState(false);
+  // Change this line to false show less by default
+  const [isExpanded, setIsExpanded] = useState(true);
 
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const handleEdit = useCallback(() => {
     setIsEditing((v) => !v);
   }, []);
 
+  const handleExpand = useCallback(() => {
+    setIsExpanded((v) => !v);
+  }, []);
+
+  const list = useMemo<ListItemsType[]>(() => {
+    const arr = [{ type: 'asset-list-header' }].concat(tokens);
+
+    const topItems: ListItemsType[] = arr
+      .slice(0, 6)
+      .concat({ type: 'asset-list-separator' });
+
+    if (!isExpanded) {
+      return topItems;
+    }
+
+    return topItems.concat(arr.slice(6, arr.length) as ListItemsType[]);
+  }, [isExpanded]);
+
+  const onItemNeeded = useWorkletCallback((index) => list[index], [list]);
+
   return (
     <View style={styles.container}>
-      <Header onEditPress={handleEdit} isEditing={isEditing} />
+      <Header />
 
       <WishList.Component
         style={styles.listContainer}
         initialIndex={0}
-        onItemNeeded={(index) => {
-          'worklet';
-          return tokens[index];
-        }}
+        onItemNeeded={onItemNeeded}
       >
-        <WishList.Template type="list-header">
+        <WishList.Template type="asset-list-header">
           <AssetListHeader />
+        </WishList.Template>
+        <WishList.Template type="asset-list-separator">
+          <AssetListSeparator
+            isEditing={isEditing}
+            onExpand={handleExpand}
+            isExpanded={isExpanded}
+          />
         </WishList.Template>
         <WishList.Template type="asset">
           <AssetItem />
