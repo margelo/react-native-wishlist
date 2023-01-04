824d2b97bf75abdb53bab4fedf081d2d66da4b42
chrfalch <christian.falch@gmail.com>
2023-01-04 12:45:01 -0800

    Added export as component / template
    Created template for defining children as templates inside WishList component

--- Wishlist/src/WishList.tsx
+++ Wishlist/src/WishList.tsx
@@ -1,42 +1,93 @@
-import React, { useEffect, useCallback, useRef } from 'react';
-import { View, Dimensions } from 'react-native';
-import NativeWishList from './NativeComponent';
-import InflatorRepository from './InflatorRepository';
-import { initEventHandler } from './EventHandler';
+import React, { useEffect, useMemo, useRef } from "react";
+import { View, Dimensions } from "react-native";
+import NativeWishList from "./NativeComponent";
+import InflatorRepository from "./InflatorRepository";
+import { initEventHandler } from "./EventHandler";
 
-const SCREEN_WIDTH = Dimensions.get('window').width;
+const SCREEN_WIDTH = Dimensions.get("window").width;
 
-export default function createWishList() {
-  const componentsRegistry = new Map<string, React.ReactElement>();
-  componentsRegistry.set("__offsetComponent", (
-    <View style={{height: 0, width: SCREEN_WIDTH}}/>
-  ));
-
-  function WishList(props) {
-    initEventHandler();
-    const { inflateItem } = props;
-    const inflatorId = useRef(null);
-
-    if (inflatorId.current == null) {
-      inflatorId.current = Math.random().toString();
-      InflatorRepository.register(inflatorId.current, inflateItem);
-    }
-
-    useEffect(() => (() => {
-      InflatorRepository.unregister(inflatorId.current);
-    }), []);
-    
-    return (
-      <NativeWishList  {...props} removeClippedSubviews={false} inflatorId={inflatorId.current} kkk={6} names={Array.from(componentsRegistry.keys())} >
-        { Array.from(componentsRegistry.values())}
-      </NativeWishList>
+function Component(props) {
+  const componentsRegistry = useMemo(() => {
+    const retVal = new Map<string, React.ReactElement>();
+    retVal.set(
+      "__offsetComponent",
+      <View style={{ height: 0, width: SCREEN_WIDTH }} />
     );
+    return retVal;
+  }, []);
+  initEventHandler();
+  const { inflateItem, children } = props;
+  const inflatorId = useRef<string | null>(null);
+  const mounted = useRef(false);
+
+  if (inflatorId.current == null) {
+    inflatorId.current = Math.random().toString();
+    InflatorRepository.register(inflatorId.current!, inflateItem);
   }
 
-  return {
-    Component: WishList,
-    registerComponent: (name: string, component: React.ReactElement) => {
-      componentsRegistry.set(name, component);
-    }
+  const updateChildTemplates = () => {
+    // Children changed.
+    Array.from(componentsRegistry.keys()).forEach((k) => {
+      if (k !== "__offsetComponent") {
+        componentsRegistry.delete(k);
+      }
+    });
+    React.Children.forEach(children, (c) => {
+      if (c.type.displayName === "WishListTemplate") {
+        componentsRegistry.set(
+          c.props.type,
+          React.Children.only(c.props.children)
+        );
+      }
+    });
+  };
+
+  useEffect(
+    () => () => {
+      inflatorId.current && InflatorRepository.unregister(inflatorId.current);
+    },
+    []
+  );
+
+  if (!mounted.current) {
+    updateChildTemplates();
   }
+
+  useEffect(() => {
+    if (mounted.current) {
+      updateChildTemplates();
+    }
+    mounted.current = true;
+  }, [children]);
+
+  const keys = Array.from(componentsRegistry.keys());
+
+  return (
+    <NativeWishList
+      {...props}
+      removeClippedSubviews={false}
+      inflatorId={inflatorId.current}
+      kkk={6}
+      names={keys}
+    >
+      {Array.from(componentsRegistry.values()).map((c, i) => (
+        <View key={keys[i]}>{c}</View>
+      ))}
+    </NativeWishList>
+  );
 }
+type TemplateProps = {
+  type: string;
+  children: React.ReactElement;
+};
+
+const Template: React.FC<TemplateProps> = ({ children }) => {
+  return children;
+};
+
+Template.displayName = "WishListTemplate";
+
+export const WishList = {
+  Component,
+  Template,
+};
