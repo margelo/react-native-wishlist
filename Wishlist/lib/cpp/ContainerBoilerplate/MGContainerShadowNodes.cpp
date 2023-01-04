6b50a6abf7f81eebbed767e22fb670a7b1fc1bf3
Szymon <szymon20000@gmail.com>
2023-01-04 12:45:02 -0800

    intercept templates using props

--- Wishlist/lib/cpp/ContainerBoilerplate/MGContainerShadowNodes.cpp
+++ Wishlist/lib/cpp/ContainerBoilerplate/MGContainerShadowNodes.cpp
@@ -9,11 +9,39 @@
  */
 
 #include <react/renderer/components/view/ConcreteViewShadowNode.h>
+#include "MGContainerShadowNodes.h"
+#include "MGContainerProps.h"
+#include <react/renderer/core/ComponentDescriptor.h>
 
 namespace facebook {
 namespace react {
 
 extern const char MGTemplateContainerComponentComponentName[] = "MGTemplateContainerComponent";
+
+MGTemplateContainerComponentShadowNode::MGTemplateContainerComponentShadowNode(
+    ShadowNodeFragment const &fragment,
+    ShadowNodeFamily::Shared const &family,
+    ShadowNodeTraits traits)
+    : ConcreteViewShadowNode(fragment, family, traits) {
+
+}
+
+MGTemplateContainerComponentShadowNode::MGTemplateContainerComponentShadowNode(
+    ShadowNode const &sourceShadowNode,
+    ShadowNodeFragment const &fragment)
+    : ConcreteViewShadowNode(sourceShadowNode, fragment) {
+}
+
+void MGTemplateContainerComponentShadowNode::appendChild(ShadowNode::Shared const &childNode) {
+   this->templates.push_back(childNode);
+   auto props = std::dynamic_pointer_cast<const MGTemplateContainerComponentProps>(this->getProps());
+   if (props->names.size() == this->templates.size()) { // last Child
+       PropsParserContext propsParserContext{getSurfaceId(), *this->getComponentDescriptor().getContextContainer().get()};
+       std::shared_ptr<MGTemplateContainerComponentProps> newProps = std::make_shared<MGTemplateContainerComponentProps>(propsParserContext, props, nullptr);
+       newProps->templates = templates;
+       props_ = newProps;
+   }
+}
 
 } // namespace react
 } // namespace facebook
