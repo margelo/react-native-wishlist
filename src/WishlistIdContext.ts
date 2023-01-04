feabdbe6ce8078bffa50a32fa77aa743ef68380a
Szymon <szymon20000@gmail.com>
2023-01-04 12:45:07 -0800

    feat: UI only like

--- src/WishlistIdContext.ts
+++ src/WishlistIdContext.ts
@@ -2,7 +2,7 @@ import { createContext, useContext } from 'react';
 
 export const WishlistIDContext = createContext<{
   id: string;
-}>();
+}>({id: 'defaultId'});
 
 export function useWishlistId() {
   const context = useContext(WishlistIDContext);
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export { useTemplateValue, TemplateValue } from './TemplateValue';
 export { createTemplateComponent } from './createTemplateComponent';
 export { WishList } from './WishList';
+export { useMarkItemsDirty } from './OrchestratorBinding';
