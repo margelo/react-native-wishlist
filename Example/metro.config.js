1011ddbb952f0bede31efdc25d1ad8f7d6113b19
Szymon <szymon20000@gmail.com>
2023-01-04 12:45:01 -0800

    use rnx

--- Example/metro.config.js
+++ Example/metro.config.js
@@ -5,13 +5,12 @@
  * @format
  */
 
-module.exports = {
-  transformer: {
-    getTransformOptions: async () => ({
-      transform: {
-        experimentalImportSupport: false,
-        inlineRequires: true,
-      },
-    }),
+const { makeMetroConfig } = require("@rnx-kit/metro-config");
+const MetroSymlinksResolver = require("@rnx-kit/metro-resolver-symlinks");
+
+module.exports = makeMetroConfig({
+  projectRoot: __dirname,
+  resolver: {
+    resolveRequest: MetroSymlinksResolver(),
   },
-};
+});
