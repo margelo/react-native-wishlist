954aec829d5d0baac9243575692426d8833676bc
Szymon <szymon20000@gmail.com>
2023-01-04 12:45:01 -0800

    done

--- Example/App.tsx
+++ Example/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import { InteractionManager } from 'react-native';
-import { View, Text } from 'react-native';
+import {InteractionManager} from 'react-native';
+import {View, Text, Image} from 'react-native';
 import createWishlist from 'root/Wishlist';
 
 const WishList = createWishlist();
@@ -13,64 +13,83 @@ remaining essentially unchanged.
 It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, 
 and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.`;
 
-const authors = [
-  'John', 'Bob', 'Szymon', 'Marc', 'Elon Musk'
-];
+const authors = ['John', 'Bob', 'Szymon', 'Marc', 'Elon Musk'];
 
-WishList.registerComponent("type1", (
-  <View style={{flexDirection: 'row-reverse'}} >
-    <View style={{margin: 10, width: '70%', backgroundColor: '#00008B'}} >
-      <View style={{margin: 5}} > 
-        <Text style={{color: 'white'}} wishId='author'> me </Text>
+WishList.registerComponent('type1', (
+  <View style={{flexDirection: 'row-reverse'}}>
+    <View style={{margin: 10, width: '70%', backgroundColor: '#00008B'}}>
+      <Image
+        style={{width: 100, height: 100}}
+        source={{
+          uri: 'https://picsum.photos/100'
+        }}
+      />
+      <View style={{margin: 5}}>
+        <Text style={{color: 'white'}} wishId="author"> me </Text>
       </View>
       <View style={{margin: 5}}>
-        <Text style={{color: 'white'}} wishId='content'> simple Message </Text>
+        <Text style={{color: 'white'}} wishId="content"> simple Message </Text>
       </View>
     </View>
   </View>
 ));
 
-WishList.registerComponent("type2", (
-  <View>
-    <View style={{margin: 10, width: '70%', backgroundColor: '#6495ED'}} >
-      <View style={{margin: 5}} > 
-        <Text style={{color: 'white'}} wishId='author'> author </Text>
-      </View>
-      <View style={{margin: 5}}>
-        <Text style={{color: 'white'}} wishId='content'> simple Message </Text>
+const Type: React.FC<{}> = () => {
+  return (
+    <View>
+      <View style={{margin: 10, width: '70%', backgroundColor: '#6495ED'}}>
+        <Image
+          style={{width: 100, height: 100}}
+          source={{
+            uri: 'https://picsum.photos/100',
+          }}
+        />
+        <View wishId="sth" style={{margin: 5}}>
+          <Text style={{color: 'white'}} wishId="author"> author </Text>
+        </View>
+        <View style={{margin: 5}}>
+          <Text style={{color: 'white'}} wishId="content"> simple Message </Text>
+        </View>
       </View>
     </View>
-  </View>
+  );
+};
+
+WishList.registerComponent('type2', (
+  <Type/>
 ));
 
 export default function App() {
   return (
     <View style={{borderWidth: 4, borderColor: 'purple', flex: 1}}>
-      {<WishList.Component 
+      <WishList.Component
       inflateItem={(index, pool) => {
-        'worklet'
-        
-        if (index < 0 || index > 1000) return undefined;
+        'worklet';
+          if (index < 0 || index > 1000) {return undefined;}
+          let imgSource = 'https://picsum.photos/seed/picsumme/100';
 
-        const type = (index % 2) + 1;
-        const item = pool.getComponent(`type${type}`);
+          const type = (index % 2) + 1;
+          const item = pool.getComponent(`type${type}`);
 
-        const len = SampleText.length;
-        const start = Math.floor(Math.random() * len);
-        const messageLen = Math.floor(Math.random() * (len - start - 1)) + 1; 
-        const newMessage = SampleText.slice(start, start + messageLen);
+          const len = SampleText.length;
+          const start = Math.floor(Math.random() * len);
+          const messageLen = Math.floor(Math.random() * (len - start - 1)) + 1;
+          const newMessage = SampleText.slice(start, start + messageLen);
 
-        if (type === 2) {
-          const randomIndex = Math.floor(Math.random() * authors.length);
-          const randomAuthor = authors[randomIndex];
-          item.View.View.Paragraph.RawText.addProps({text: randomAuthor});
-        }
-        
-        item.View.View.at(1).Paragraph.RawText.addProps({text: newMessage});
+          if (type === 2) {
+            const randomIndex = Math.floor(Math.random() * authors.length);
+            const randomAuthor = authors[randomIndex];
+            item.View.View.Paragraph.RawText.addProps({text: randomAuthor});
+            imgSource = `https://picsum.photos/seed/picsum${authors[randomIndex]}/100`;
+          }
 
-        return item;
-      }}
-    style={{flex:1}}/>}
+          item.View.View.at(1).Paragraph.RawText.addProps({text: newMessage});
+          item.View.Image.addProps({source: {uri: imgSource}});
+
+          return item;
+        }}
+        style={{flex: 1}}
+      />
     </View>
   );
 }
