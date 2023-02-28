d60476a0cf932a94e4e65b63e686176911919141
Janic Duplessis <janicduplessis@gmail.com>
2023-02-28 17:50:59 -0500

    Use real width / height

--- android/src/main/java/com/wishlist/Wishlist.kt
+++ android/src/main/java/com/wishlist/Wishlist.kt
@@ -5,30 +5,53 @@ import com.facebook.react.uimanager.FabricViewStateManager
 import com.facebook.react.views.scroll.ReactScrollView
 
 class Wishlist(reactContext: Context) :
-    ReactScrollView(reactContext), FabricViewStateManager.HasFabricViewStateManager {
+  ReactScrollView(reactContext), FabricViewStateManager.HasFabricViewStateManager {
   var inflatorId: String? = null
   var wishlistId: String? = null
   var initialIndex: Int = 0
   private var orchestrator: Orchestrator? = null
+  private var templatesRef: Int? = null
+  private var names: List<String>? = null
 
   fun setTemplates(templatesRef: Int, names: List<String>) {
+    this.templatesRef = templatesRef
+    this.names = names
+    renderIfReady()
+  }
+
+  private fun renderIfReady() {
+    val templatesRef = this.templatesRef
+    val inflatorId = this.inflatorId
+    val names = this.names
+    if (names == null || templatesRef == null || inflatorId == null || width == 0 || height == 0) {
+      return
+    }
+
     var orchestrator = this.orchestrator
     if (orchestrator == null) {
       orchestrator =
-          Orchestrator(
-              wishlistId!!, fabricViewStateManager.stateData!!.getInt("viewportCarer"))
+        Orchestrator(
+          wishlistId!!, fabricViewStateManager.stateData!!.getInt("viewportCarer")
+        )
       this.orchestrator = orchestrator
     }
     // TODO: width / height
     orchestrator.renderAsync(
-        300f, 600f, 0f, initialIndex, templatesRef, names, inflatorId!!)
+      width.toFloat(), height.toFloat(), 0f, initialIndex, templatesRef, names, inflatorId
+    )
   }
 
-  fun scrollToItem(index: Int, animated: Boolean) {}
+  override fun onLayout(changed: Boolean, l: Int, t: Int, r: Int, b: Int) {
+    super.onLayout(changed, l, t, r, b)
+
+    renderIfReady()
+  }
 
   override fun onScrollChanged(x: Int, y: Int, oldX: Int, oldY: Int) {
     super.onScrollChanged(x, y, oldX, oldY)
 
-    orchestrator?.didScrollAsync(300f, 600f, y.toFloat(), inflatorId!!)
+    orchestrator?.didScrollAsync(width.toFloat(), height.toFloat(), y.toFloat(), inflatorId!!)
   }
+
+  fun scrollToItem(index: Int, animated: Boolean) {}
 }
