2ac8628f3279859dafe58244cbb17f06a46a81e0
Janic Duplessis <janicduplessis@gmail.com>
2023-03-04 14:51:18 -0500

    Hook up events

--- android/src/main/jni/WishlistManagerModule.cpp
+++ android/src/main/jni/WishlistManagerModule.cpp
@@ -4,25 +4,57 @@
 #include "MGUIManagerHolder.h"
 #include "WishlistJsRuntime.h"
 
-using namespace facebook;
+using namespace facebook::react;
+using namespace facebook::jni;
 
 namespace Wishlist {
 
-jni::local_ref<WishlistManagerModule::jhybriddata>
-WishlistManagerModule::initHybrid(jni::alias_ref<jclass>) {
+local_ref<WishlistManagerModule::jhybriddata> WishlistManagerModule::initHybrid(
+    alias_ref<jclass>) {
   return makeCxxInstance();
 }
 
 WishlistManagerModule::WishlistManagerModule() {}
 
+WishlistManagerModule::~WishlistManagerModule() {
+  scheduler_->removeEventListener(eventListener_);
+}
+
 void WishlistManagerModule::nativeInstall(
     jlong jsiRuntimeRef,
-    jni::alias_ref<react::CallInvokerHolder::javaobject> jsCallInvokerHolder,
-    jni::alias_ref<react::JFabricUIManager::javaobject> fabricUIManager) {
-  auto jsiRuntime{reinterpret_cast<jsi::Runtime *>(jsiRuntimeRef)};
-  auto jsCallInvoker = jsCallInvokerHolder->cthis()->getCallInvoker();
+    alias_ref<CallInvokerHolder::javaobject> jsCallInvokerHolder,
+    alias_ref<JFabricUIManager::javaobject> fabricUIManager) {
+  auto jsiRuntime{reinterpret_cast<Runtime *>(jsiRuntimeRef)};
+  auto const &jsCallInvoker = jsCallInvokerHolder->cthis()->getCallInvoker();
+  scheduler_ = fabricUIManager->getBinding()->getScheduler();
 
-  _wishlistQueue = std::make_shared<RNWorklet::DispatchQueue>("wishlistqueue");
+  eventListener_ =
+      std::make_shared<EventListener>([this](const RawEvent &event) -> bool {
+        if (event.eventTarget == nullptr) {
+          return false;
+        }
+
+        WishlistJsRuntime::getInstance().accessRuntime([this,
+                                                        event](Runtime &rt) {
+          try {
+            auto handleEvent = rt.global()
+                                   .getPropertyAsObject(rt, "global")
+                                   .getPropertyAsFunction(rt, "handleEvent");
+            handleEvent.call(
+                rt,
+                event.type,
+                event.eventTarget->getTag(),
+                event.payloadFactory(rt));
+          } catch (std::exception &error) {
+            errorHandler_->reportError(error.what());
+          }
+        });
+
+        return true;
+      });
+  scheduler_->addEventListener(eventListener_);
+
+  wishlistQueue_ = std::make_shared<RNWorklet::DispatchQueue>("wishlistqueue");
 
   WishlistJsRuntime::getInstance().initialize(
       jsiRuntime,
@@ -30,12 +62,10 @@ void WishlistManagerModule::nativeInstall(
         jsCallInvoker->invokeAsync(std::move(f));
       },
       [=](std::function<void()> &&f) {
-        _wishlistQueue->dispatch(std::move(f));
+        wishlistQueue_->dispatch(std::move(f));
       });
 
-  auto uiManager =
-      fabricUIManager->getBinding()->getScheduler()->getUIManager();
-  MGUIManagerHolder::getInstance().setUIManager(std::move(uiManager));
+  MGUIManagerHolder::getInstance().setUIManager(scheduler_->getUIManager());
 }
 
 void WishlistManagerModule::registerNatives() {
--- android/src/main/jni/WishlistManagerModule.hpp
+++ android/src/main/jni/WishlistManagerModule.hpp
@@ -4,14 +4,19 @@
 #include <fbjni/fbjni.h>
 #include <react-native-worklets/WKTDispatchQueue.h>
 #include <react/fabric/JFabricUIManager.h>
+#include <react/renderer/core/EventListener.h>
+#include <react/renderer/scheduler/Scheduler.h>
+#include "ErrorHandlerAndroid.h"
 
-using namespace facebook;
+using namespace facebook::react;
+using namespace facebook::jni;
 
 namespace Wishlist {
 
-class WishlistManagerModule : public jni::HybridClass<WishlistManagerModule> {
+class WishlistManagerModule : public HybridClass<WishlistManagerModule> {
  public:
   WishlistManagerModule();
+  ~WishlistManagerModule();
 
   static constexpr auto kJavaDescriptor =
       "Lcom/wishlist/WishlistManagerModule;";
@@ -19,17 +24,20 @@ class WishlistManagerModule : public jni::HybridClass<WishlistManagerModule> {
   static void registerNatives();
 
  private:
-  static jni::local_ref<jhybriddata> initHybrid(jni::alias_ref<jclass>);
+  static local_ref<jhybriddata> initHybrid(alias_ref<jclass>);
 
   void nativeInstall(
       jlong jsiRuntimeRef,
-      jni::alias_ref<react::CallInvokerHolder::javaobject> jsCallInvokerHolder,
-      jni::alias_ref<react::JFabricUIManager::javaobject> fabricUIManager);
+      alias_ref<CallInvokerHolder::javaobject> jsCallInvokerHolder,
+      alias_ref<JFabricUIManager::javaobject> fabricUIManager);
 
  private:
   friend HybridBase;
 
-  std::shared_ptr<RNWorklet::DispatchQueue> _wishlistQueue;
+  std::shared_ptr<RNWorklet::DispatchQueue> wishlistQueue_;
+  std::shared_ptr<Scheduler> scheduler_;
+  std::shared_ptr<EventListener> eventListener_;
+  std::shared_ptr<ErrorHandlerAndroid> errorHandler_;
 };
 
 } // namespace Wishlist
